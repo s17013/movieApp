7305c68aa54b6deca7e42fb2b91c50ddda22c796
s17013 <s17013@std.it-college.ac.jp>
2018-02-08 14:00:04 +0900

    middle

---
+++ App (コピー).js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './App.css';
+import Movie from './Movie';
+
+
+
+class App extends Component {
+
+  state = {
+  greeting:'hello!',
+
+ movies : [
+{
+  title:"Matrix",
+  poster:"http://cfile230.uf.daum.net/image/120165194C241D693330B4"
+},
+{
+  title:"Full Metal jacket",
+  poster:"http://blogthumb2.naver.net/data18/2007/1/6/4/full_metal_jacket_manif-inde9898.jpg?type=w2",
+},
+{
+  title:"Old Boy",
+  poster:"https://www.languagetrainers.com/reviews/foreign-film-reviews/uploads/9214-Oldboy.jpg",
+},
+{
+  title:"Star Wars",
+  poster:"http://moonhak.co.kr/home/wp-content/uploads/bookcover/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88-%EC%94%A8%EB%84%A4%EC%95%84%ED%8A%B84_%ED%91%9C1_web.jpg"
+    }
+  ]
+}
+
+componentDidMount(){
+  setTimeout(() => {
+    this.setState({
+    movie : [
+      ...this.state.movies,
+      {
+             title:"GodFather",
+             poster:"https://images-na.ssl-images-amazon.com/images/M/MV5BY2Q2NzQ3ZDUtNWU5OC00Yjc0LThlYmEtNWM3NTFmM2JiY2VhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR3,0,182,268_AL_.jpg"
+ }
+]
+
+    })
+  }, 1000)
+}
+
+
+  render() {
+    return (
+      <div className="App">
+      {this.state.movies.map((movie, index) => {
+       return <Movie title={movie.title} poster={movie.poster} key={index}/>
+      })}
+      </div>
+    );
+  }
+}
+
+export default App;
--- App.js
+++ App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
+
+
+class App extends Component {
+
+  state = {
+
+}
+
+componentDidMount(){
+  setTimeout(() => {
+    this.setState({
+    movies : [
 {
   title:"Matrix",
-  poster:"https://metrouk2.files.wordpress.com/2017/02/the-matrix.jpg?quality=80&strip=all&strip=all"
+  poster:"http://cfile230.uf.daum.net/image/120165194C241D693330B4"
 },
 {
   title:"Full Metal jacket",
-  poster:"https://images-na.ssl-images-amazon.com/images/I/81trsp4%2BMEL._SL1500_.jpg",
+  poster:"http://blogthumb2.naver.net/data18/2007/1/6/4/full_metal_jacket_manif-inde9898.jpg?type=w2",
 },
 {
   title:"Old Boy",
@@ -17,18 +28,29 @@ const movies = [
 },
 {
   title:"Star Wars",
-  poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdbzzWAC-uDjLNaxk_YbW_CL_LExZRO6XKEh1xw76Nm7wHWRmhoQ"
-  }
+  poster:"http://moonhak.co.kr/home/wp-content/uploads/bookcover/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88-%EC%94%A8%EB%84%A4%EC%95%84%ED%8A%B84_%ED%91%9C1_web.jpg",
+},
+{
+  title:"GodFather",
+  poster:"https://images-na.ssl-images-amazon.com/images/M/MV5BY2Q2NzQ3ZDUtNWU5OC00Yjc0LThlYmEtNWM3NTFmM2JiY2VhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR3,0,182,268_AL_.jpg"
+}
+      ]
 
-]
+    })
+  }, 5000)
+}
 
-class App extends Component {
+_renderMovies = () => {
+ const movies =  this.state.movies.map((movie, index) =>{
+       return <Movie title={movie.title} poster={movie.poster} key={index} / >
+  })
+ return movies
+}
+
   render() {
     return (
       <div className="App">
-      {movies.map((movie, index) => {
-       return <Movie title={movie.title} poster={movie.poster} key={index}/>
-      })}
+      {this.state.movies ? this._renderMovies(): 'Loading'}
       </div>
     );
   }
